177b699ce2ffad055c450ac59896c5fe82dc6c4c
tin <tingeber@gmail.com>
2024-05-09 13:19:48 +0200

    wezterm tweaks

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,29 +1,33 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local mux = wezterm.mux
+
+wezterm.on('gui-startup', function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
-config.colors = {
-	foreground = "#CBE0F0",
-	background = "#011423",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#706b4e",
-	selection_fg = "#f3d9c4",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
-}
+config.color_scheme = 'Catppuccin Frappe'
+
+
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 14
 config.line_height = 1.1
 
+
+config.default_cursor_style = 'BlinkingBlock'
+-- Acceptable values are SteadyBlock, BlinkingBlock, SteadyUnderline, BlinkingUnderline, SteadyBar, and BlinkingBar
+
 -- config.enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = true
 
 config.window_decorations = "RESIZE"
 -- config.window_background_opacity = 0.9
